1538993063f1081104aad0f65aa5d39333aa07d3
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-05-26 14:25:52 +0000

    Add script to organize validation images

---
+++ organize_val_imgs.sh
@@ -0,0 +1,12 @@
+cd tiny-imagenet-200/val;
+export IFS="	"
+while read img class x1 y1 x2 y2; do
+    if [ ! -d "$class" ]; then
+        mkdir $class
+    fi
+done < val_annotations.txt
+
+while read img class x1 y1 x2 y2; do
+    mv "images/$img" $class
+done < val_annotations.txt
+
